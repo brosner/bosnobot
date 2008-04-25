7fde1c7459b5838f5150889016e143f0591a09bf
brosner <brosner@gmail.com>
2008-04-25 14:36:03 -0600

    Added some more methods to the ChannelPool.

--- bosnobot/pool.py
+++ bosnobot/pool.py
@@ -3,6 +3,14 @@ class ChannelPool(object):
     """
     Handles a pool of IRC channels that the bot will use.
     """
-    def __init__(self, channels={}):
+    def __init__(self, channels=[]):
         for channel in channels:
             self.add(channel)
+    
+    def __iter__(self):
+        for channel in self.channels.itervalues():
+            yield channel
+    
+    def add(self, channel):
+        # use a dict to speed up lookups
+        self.channels[channel.name] = channel
