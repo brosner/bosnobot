534d932d1b640afe67648abbe730da06cc94554c
brosner <brosner@gmail.com>
2008-04-26 17:00:53 -0600

    Added a setup.py script.

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+
+from distutils.core import setup
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == "":
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+bosnobot_dir = "bosnobot"
+
+packages = []
+for dirpath, dirnames, filenames in os.walk(bosnobot_dir):
+    # ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    if "__init__.py" in filenames:
+        packages.append(".".join(fullsplit(dirpath)))
+
+setup(
+    name = "bosnobot",
+    version = "1.0-beta",
+    description = "Python IRC bot",
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    url = "http://github.com/brosner/bosnobot/",
+    packages = packages,
+)
