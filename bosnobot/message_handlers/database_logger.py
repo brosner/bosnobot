a23ba7830244d792d683b79b13f48d5172fc6f9c
brosner <brosner@gmail.com>
2008-05-03 08:38:44 -0600

    Added some more columns to the DatabaseLogger. Also added a method to output the creation SQL.

--- bosnobot/message_handlers/database_logger.py
+++ bosnobot/message_handlers/database_logger.py
@@ -1,8 +1,12 @@
 
+from datetime import datetime
+from cStringIO import StringIO
+
 from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.orm import sessionmaker, mapper
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import sessionmaker, mapper, relation
 from bosnobot.conf import settings
+from bosnobot.channel import Channel
 from bosnobot.message import Message
 
 class DatabaseLogger(object):
@@ -13,15 +17,37 @@ class DatabaseLogger(object):
         self.session = sessionmaker(bind=self.engine)()
     
     def setup_tables(self):
+        channels_table = Table("irc_channel", self.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("name", String(50)))
+        mapper(Channel, channels_table, properties={
+            "messages": relation(Message, backref="channel"),
+        })
+        
         messages_table = Table("irc_message", self.metadata,
             Column("id", Integer, primary_key=True),
-            Column("text", Text))
+            Column("channel_id", ForeignKey("irc_channel.id")),
+            Column("nickname", String(19)),
+            Column("text", Text),
+            Column("logged", DateTime, default=datetime.now),
+            Column("is_action", Boolean),
+            Column("is_blocked", Boolean))
         mapper(Message, messages_table, properties={
             "message": messages_table.c.text,
         })
         
         # setup tables
         self.metadata.create_all()
+    
+    def as_sql(self):
+        buf = StringIO()
+        def buffer_output(s, p=""):
+            return buf.write(s + p)
+        engine = create_engine(settings.DATABASE_URI,
+            strategy="mock",
+            executor=buffer_output)
+        self.metadata.create_all(engine)
+        print buf.getvalue()
         
     def process_message(self, message):
         self.session.save(message)
