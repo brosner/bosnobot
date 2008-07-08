041d6c5000ef6c76413c04c50604fc9c614a802b
brosner <brosner@gmail.com>
2008-07-08 13:27:58 -0600

    Added a simple FileLogger. It needs more work.

---
+++ bosnobot/message_handlers/simple.py
@@ -0,0 +1,16 @@
+
+import sys
+
+from bosnobot.conf import settings
+
+class FileLogger(object):
+    def __init__(self):
+        if settings.FILE_LOGGER_FILENAME == "stdout":
+            self.stream = sys.stdout
+        else:
+            self.stream = open(settings.FILE_LOGGER_FILENAME)
+    
+    def process_message(self, message):
+        self.stream.write(str(message))
+        self.stream.write("\n")
+        self.stream.flush()
