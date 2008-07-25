16f2c0b4842fe2df2a7d940f0001977d93f4365a
brosner <brosner@gmail.com>
2008-07-25 15:19:41 -0600

    Message handlers can now safely send messages back to the main reactor thread using call_from_thread. Not as clean as I like it to be.

--- bosnobot/channel.py
+++ bosnobot/channel.py
@@ -1,4 +1,6 @@
 
+from twisted.internet import reactor
+
 class Channel(object):
     """
     Represents an IRC channel.
@@ -11,8 +13,11 @@ class Channel(object):
     def __repr__(self):
         return "<Channel: %s>" % self.name
     
-    def msg(self, message):
+    def msg(self, message, call_from_thread=False):
         """
         Tell the bot to send a message to this channel.
         """
-        self.bot.msg(self.name, message)
+        if call_from_thread:
+            reactor.callFromThread(self.bot.msg, self.name, message)
+        else:
+            self.bot.msg(self.name, message)
