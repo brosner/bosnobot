1016918b6c9c10842b645d638dd4ce366e748240
brosner <brosner@gmail.com>
2008-07-28 14:03:40 -0600

    Added an IrcBotService for proper usage with twistd.

--- bosnobot/__init__.py
+++ bosnobot/__init__.py
@@ -5,14 +5,20 @@ import threading
 
 from twisted.python import log
 from twisted.internet import reactor
+from twisted.application import internet
 
 from bosnobot.conf import settings
 from bosnobot.bot import IrcBotFactory
 from bosnobot.log import PythonLoggingObserver
 
+class IrcBotService(internet.TCPClient):
+    def __init__(self, *args, **kwargs):
+        factory = IrcBotFactory(settings.BOT, settings.BOT_CHANNELS)
+        internet.TCPClient.__init__(self,
+            settings.BOT_IRC_SERVER, settings.BOT_IRC_PORT, factory)
+
 def main_loop():
     factory = IrcBotFactory(settings.BOT, settings.BOT_CHANNELS)
-    
     reactor.connectTCP(settings.BOT_IRC_SERVER, settings.BOT_IRC_PORT, factory)
     reactor.run()
 
