c72599fd955f9ad0e5acaa2ac4c27373c7d8acd4
brosner <brosner@gmail.com>
2008-10-03 18:13:10 -0600

    Attempting to fix socket timeout errors.

--- bosnobot/event_handlers/couchdb_logger.py
+++ bosnobot/event_handlers/couchdb_logger.py
@@ -7,10 +7,10 @@ from bosnobot.message import Message
 class CouchDBLogger(object):
     def __init__(self):
         self.server = couchdb.Server("http://127.0.0.1:5984/")
-        self.database = self.server["irc_events"]
     
     def process_event(self, event):
+        database = self.server["irc_events"]
         document = couchdb.Document(**dict(event.as_dict(), **{
             "logged": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         }))
-        self.database.create(document)
+        database.create(document)
